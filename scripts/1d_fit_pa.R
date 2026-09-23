714ee884a4ea2e8255f9108cf1b25c9b890b7159
enazario11 <enazario@ucsc.edu>
2026-09-23 14:22:08 -0700

    subsample PAs and save

--- scripts/1d_fit_pa.R
+++ scripts/1d_fit_pa.R
@@ -9,9 +9,10 @@ library(aniMotum)
 source(here("functions/keep_windows.R"))
 set.seed(0902)
 
-#TO DO 
-#4) get pseudo-absences to a 1:1 ratio.
-#5) Run albacore SSM and then generate PAs
+#TO DO
+# 1) finalize study domain boundary to filter locs and PAs 
+#2) get pseudo-absences to a 1:1 ratio.
+#3) Run albacore SSM and then generate PAs
 
 # land file
 land <- ne_countries(scale = "large", returnclass = "sf") %>% st_make_valid() 
@@ -73,7 +74,7 @@ swo_pa_r <- route_path(swo_pa_filt, centroids = TRUE)
 plot(swo_pa_r[1,])
 saveRDS(swo_pa_r, here("data/loc_data/processed/pa/swo_pa_routed.rds"))
 
-#### remove locations in gap windows, on land, and outside of MOM6 domain ####
+#### remove locations in gap windows, on land, and outside of study domain ####
 #combine rerouted presences and PAs into one df, then make locs a spatial feature
 #albacore 
 
@@ -149,13 +150,14 @@ swo_no_gaps <- swo %>%
   inner_join(bind_rows(swo_windows), 
              by = join_by(id, between(date, start_time, end_time)))
 
-
 #### Land #####
+land_union <- st_union(land) #speeds up st_filter
+
 #albacore 
 
 
 #blue sharks
-blu_no_land <- st_difference(blu_no_gaps, land)
+blu_no_land <- st_filter(blu_no_gaps, land_union, .predicate = st_disjoint)
 
 ggplot() + 
     geom_sf(data = land, fill = "grey85", color = "grey30", linewidth = 0.2) +
@@ -166,41 +168,140 @@ ggplot() +
     theme_bw() 
 
 #mako sharks
-
-
-#swordfish
-
-
-#### MOM6 #####
-#get domain and turn into sf object
-mom_domain <- rast(here("../MI_AGI/data/enviro/nep/temp/raw/tob.nep.full.hcast.monthly.regrid.r20250912.199301-202506"))
-mom_domain <- mom_domain[1]
-
-mom_poly <- st_as_sf(as.polygons(mom_domain))
-
-#albacore
-
-
-#blue sharks
-blu_mom <- st_intersection(blu_no_land, mom_poly)
+mako_no_land <- st_filter(mako_no_gaps, land_union, .predicate = st_disjoint)
 
 ggplot() + 
     geom_sf(data = land, fill = "grey85", color = "grey30", linewidth = 0.2) +
-    geom_sf(data = blu_mom, aes(color = loc_type)) + 
+    geom_sf(data = mako_no_land, aes(color = loc_type)) + 
     coord_sf(xlim = c(-170, -100),
       ylim = c(0, 55),
       expand = FALSE) +
     theme_bw() 
 
-#mako sharks
-
-
 
 #swordfish
+swo_no_land <- st_filter(swo_no_gaps, land_union, .predicate = st_disjoint)
 
+ggplot() + 
+    geom_sf(data = land, fill = "grey85", color = "grey30", linewidth = 0.2) +
+    geom_sf(data = swo_no_land, aes(color = loc_type)) + 
+    coord_sf(xlim = c(-170, -100),
+      ylim = c(0, 55),
+      expand = FALSE) +
+    theme_bw() 
+
+#### study domain #####
+#get domain and turn into sf object -- Update once have domain boundary
+# mom_domain <- rast(here("../MI_AGI/data/enviro/nep/temp/raw/tob.nep.full.hcast.monthly.regrid.r20250912.199301-202506"))
+# mom_domain <- mom_domain[1]
+
+# mom_poly <- st_as_sf(as.polygons(mom_domain))
+
+bbox <- st_bbox(c(xmin = -170, ymin = 0, xmax = -100, ymax = 55), crs = 4326)
+
+#albacore
+
+
+#blue sharks
+blu_domain <- st_filter(blu_no_land, st_as_sfc(bbox), .predicate = st_within)
+
+ggplot() + 
+    geom_sf(data = land, fill = "grey85", color = "grey30", linewidth = 0.2) +
+    geom_sf(data = blu_domain, aes(color = loc_type)) + 
+    coord_sf(xlim = c(-175, -98),
+      ylim = c(-5, 60),
+      expand = FALSE) +
+    theme_bw() 
+
+#mako sharks
+mako_domain <- st_filter(mako_no_land, st_as_sfc(bbox), .predicate = st_within)
+
+ggplot() + 
+    geom_sf(data = land, fill = "grey85", color = "grey30", linewidth = 0.2) +
+    geom_sf(data = mako_domain, aes(color = loc_type)) + 
+    coord_sf(xlim = c(-175, -98),
+      ylim = c(-5, 60),
+      expand = FALSE) +
+    theme_bw() 
+
+#swordfish
+swo_domain <- st_filter(swo_no_land, st_as_sfc(bbox), .predicate = st_within)
+
+ggplot() + 
+    geom_sf(data = land, fill = "grey85", color = "grey30", linewidth = 0.2) +
+    geom_sf(data = swo_domain, aes(color = loc_type)) + 
+    coord_sf(xlim = c(-175, -98),
+      ylim = c(-5, 60),
+      expand = FALSE) +
+    theme_bw() 
 
 #### randomly sample PAs to get 1:1 with presences #####
+pa_ratio <- function(sp_dat, ratio = 1){
+
+  pres_abs_df <- data.frame()
+
+for(i in 1:length(unique(sp_dat$id))){
+
+  curr_id = unique(sp_dat$id)[i]
+  temp_dat = sp_dat %>% filter(id == curr_id)
+  num_presence = temp_dat %>% filter(loc_type == "presence") %>% summarise(n = n())
+  num_presence = num_presence$n
+
+  pres_df = temp_dat %>% filter(loc_type == "presence")
+  abs_df = temp_dat %>% filter(loc_type == "absence")
+  abs_df_sub = abs_df[sample(nrow(abs_df), size = num_presence*ratio, replace = FALSE), ]
+
+  pres_abs_temp <- rbind(pres_df, abs_df_sub)
+
+  pres_abs_df <- rbind(pres_abs_df, pres_abs_temp)
+
+} #end for loop
+  return(pres_abs_df)
+} #end function
+
+#albacore 
 
 
+#blue sharks
+blu_pres_abs <- pa_ratio(blu_domain)
+
+ggplot() + 
+    geom_sf(data = land, fill = "grey85", color = "grey30", linewidth = 0.2) +
+    geom_sf(data = blu_pres_abs, aes(color = loc_type), size = 2, alpha = 0.8) + 
+    coord_sf(xlim = c(-175, -98),
+      ylim = c(-5, 60),
+      expand = FALSE) +
+    theme_bw() +
+  scale_color_manual(values = c("#77ABD9", "dodgerblue4"))
+
+saveRDS(blu_pres_abs, here("data/loc_data/processed/pres_abs/blu_pres_abs.rds"))
+
+#mako sharks
+mako_pres_abs <- pa_ratio(mako_domain)
+
+ggplot() + 
+    geom_sf(data = land, fill = "grey85", color = "grey30", linewidth = 0.2) +
+    geom_sf(data = mako_pres_abs, aes(color = loc_type), size = 2, alpha = 0.8) + 
+    coord_sf(xlim = c(-175, -98),
+      ylim = c(-5, 60),
+      expand = FALSE) +
+    theme_bw() +
+  scale_color_manual(values = c("#77ABD9", "dodgerblue4"))
+
+saveRDS(mako_pres_abs, here("data/loc_data/processed/pres_abs/mako_pres_abs.rds"))
+
+#swordfish
+swo_pres_abs <- pa_ratio(swo_domain)
+
+ggplot() + 
+    geom_sf(data = land, fill = "grey85", color = "grey30", linewidth = 0.2) +
+    geom_sf(data = swo_pres_abs, aes(color = loc_type), size = 2, alpha = 0.8) + 
+    coord_sf(xlim = c(-175, -98),
+      ylim = c(-5, 60),
+      expand = FALSE) +
+    theme_bw() +
+  scale_color_manual(values = c("#77ABD9", "dodgerblue4"))
+
+saveRDS(swo_pres_abs, here("data/loc_data/processed/pres_abs/swo_pres_abs.rds"))
 
 
